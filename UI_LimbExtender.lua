108cfb886b749f0198f29f6e8632914107cfc8c5
RockStarity <mailrockstarity@gmail.com>
2026-02-20 18:25:57 +0300

    Update UI_LimbExtender.lua

--- UI_LimbExtender.lua
+++ UI_LimbExtender.lua
@@ -1,7 +1,7 @@
 local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
 
-getgenv().le = getgenv().le or loadstring(game:HttpGet('https://raw.githubusercontent.com/AAPVdev/scripts/refs/heads/main/LimbExtender.lua'))()
+getgenv().le = getgenv().le or loadstring(game:HttpGet('https://raw.githubusercontent.com/RockStarity/scripts/refs/heads/main/LimbExtender.lua'))()
 local LimbExtender = getgenv().le
 
 local le = LimbExtender({
